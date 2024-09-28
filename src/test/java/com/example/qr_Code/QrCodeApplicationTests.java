c7cf9209aa74c6f155ea043d2a081d0b4017b118
Deepak Maheshwari <115531844+Deepak86190@users.noreply.github.com>
2024-09-28 23:39:34 +0530

    Add files via upload
    f

---
+++ src/test/java/com/example/qr_Code/QrCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qr_Code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
